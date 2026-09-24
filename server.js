f6db7dd145cd6b90165a323ab60abda9f2363078
brianljett-coder <brianljett@gmail.com>
2026-09-23 22:07:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -458,9 +458,6 @@ async function classicDiscover() {
     ) {
       all.push(movie);
     }
- * OTHER COMIC HEROES
- *
- * Independent and non-Big-Two comic properties.
  async function otherComicDiscover() {
   const properties = [
     "Hellboy",
